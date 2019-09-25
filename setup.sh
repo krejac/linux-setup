d0a9f399054016db4ee725ec64d2f8e6adf67448
Kresten Jacobsen <k@jcbsn.dk>
2019-09-25 13:19:17 +0200

    powerline for vim added

--- setup.sh
+++ setup.sh
@@ -11,7 +11,9 @@ fi
 
 echo ""
 echo "*********************"
+echo "*                   *"
 echo "* Updating packages *"
+echo "*                   *"
 echo "*********************"
 echo ""
 sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y
@@ -69,7 +71,40 @@ EOL
 source ~/.bashrc
 
 cat >> ~/.vimrc << EOL
-set rtp+=$HOME/.local/lib/python2.7/site-packages/powerline/bindings/vim/
+set nocompatible              " be iMproved, required
+filetype off                  " required
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+" alternatively, pass a path where Vundle should install plugins
+"call vundle#begin('~/some/path/here')
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+
+" The following are examples of different formats supported.
+" Keep Plugin commands between vundle#begin/end.
+" plugin on GitHub repo
+" Plugin 'tpope/vim-fugitive'
+Plugin 'powerline/powerline', {'rtp': 'powerline/bindings/vim/'}
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+" To ignore plugin indent changes, instead use:
+"filetype plugin on
+"
+" Brief help
+" :PluginList       - lists configured plugins
+" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
+" :PluginSearch foo - searches for foo; append `!` to refresh local cache
+" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
+"
+" see :h vundle for more details or wiki for FAQ
+" Put your non-Plugin stuff after this line
+
+set rtp+=/home/krejac/.local/lib/python2.7/site-packages/powerline/bindings/vim/
 
 " Always show statusline
 set laststatus=2
